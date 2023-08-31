2740a37d9f68f46396d0edae36de49f231e89881
Calin Corcotoi <calin.corcotoi@veoneer.com>
2023-08-31 09:29:28 +0300

    update the responsiveness of the site

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-$(".navbar-toggler").on("hidden.bs.collapse", function () {
-  console.log("calin");
-});
-
-//TODO:finish the resize for nav bar
-$(window).resize(function () {
-  var navbar_visible = $(".navbar-toggler").is(":visible");
-  console.log(navbar_visible);
-});
+const navImgLogo = document.querySelector(".navbar__img-logo");
