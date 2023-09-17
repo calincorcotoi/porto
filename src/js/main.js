2dd74ed97bad1e9ac8c60656846c54070684c1e9
Calin Corcotoi <calin.corcotoi@veoneer.com>
2023-09-17 11:09:48 +0300

    added navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 const navImgLogo = document.querySelector(".navbar__img-logo");
+const btnSendMessage = document.querySelector(".nav-link");
+
+btnSendMessage.addEventListener("click", function (e) {
+  //   console.log("calin");
+  //   e.preventDefault();
+  //   if (e.target.classList.contains("nav__link")) {
+  //     const id = e.target.getAttribute("href");
+  //     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  //   }
+});
